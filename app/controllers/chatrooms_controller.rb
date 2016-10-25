8a9345bdc132cb3a0a4b1d0e7bcb7414961e92b7
Julien Da Silva <julien.da.silva@hotmail.com>
2016-10-25 15:00:36 +0200

    checkpoint

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,29 @@
+class ChatroomsController < ApplicationController
+
+  def index
+    @chatrooms = Chatroom.all
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.save
+    redirect_to chatrooms_path
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def destroy
+    @chatroom = Chatroom.find(params[:id])
+    @chatroom.save
+    redirect_to chatrooms_path
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:topic)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'chatrooms#index'
+
+  resources :chatrooms, only: [:index, :create, :show] do 
+    resources :message, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
